ee23102143b645ce1b9acef92e3060ed471e7761
McCray <tubach75@hotmail.com>
2021-09-10 11:26:40 -0400

    commit tree b62e9d8b31f7da4d9a83d5c0a2c3dcb92d8a9905

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,62 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addBand(this.state)
+    this.setState({name: "",})
+  }
+
+  //renderBands = () => { if (this.props.bands.length > 0) { return this.props.bands.map((band, idx) => <li key={idx}>{band.name}</li>)}}
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="add band" 
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
+        {/*this.renderBands()*/}
       </div>
     )
   }
 }
 
 export default BandInput
+
+/*
+import React from 'react';
+
+const Bands = props => {
+  const bands = props.bands.map((band, index) => {
+    return <li key={index}>{band.name}</li>;
+  });
+
+  return (
+    <div>
+      {bands}
+    </div>
+  );
+
+};
+
+export default Bands;
+*/
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import BandInput from '../components/BandInput'
+import Bands from './Bands'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <Bands bands={this.props.bands}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addBand: formData => dispatch({ type: 'ADD_BAND', payload: formData })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
+
+/*
+  import React, { Component } from "react";
+import { connect } from "react-redux";
+import Todo from "./Todo";
+
+class TodosContainer extends Component {
+
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+
+  render() {
+    return(
+      <ol>
+        {this.renderTodos()}
+      </ol>
+    );
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+
+export default connect(mapStateToProps)(TodosContainer);
+*/
